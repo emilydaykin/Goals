afddd2fee981cb047cf0c655b7174b21ef45dc73
Emily Daykin <emilydaykin31@gmail.com>
2022-09-01 10:50:19 +0200

    added simple styling via inline styling and stylesheet objects

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style='auto' />
+      <View>
+        <Text style={styles.mainText}>Another piece of text</Text>
+      </View>
+      <Text style={{ margin: 16, padding: 16, borderWidth: 1, borderColor: 'orange' }}>
+        Hello World!
+      </Text>
+      <Button title='click' />
     </View>
   );
 }
@@ -16,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mainText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 3,
+    borderColor: 'purple'
   }
 });
